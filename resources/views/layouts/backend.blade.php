6111d34b431007b38ed72fa724ff96a776149b52
snarksliveshere <snarksliveshere@gmail.com>
2018-09-01 17:58:20 +0300

    restructure layouts & nav inherits

---
+++ resources/views/layouts/backend.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('nav')
+    @include('partitials.nav.backend')
+@endsection
+@section('content')
+@endsection
---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+<div class="container">
+    @section('nav')
+        @include('partitials.nav.frontend')
+    @endsection
+    @section('content')
+    @endsection
+</div>
