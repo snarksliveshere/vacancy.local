f97eb229c4b02b901d2a4f49141cfdb40a9af44b
snarksliveshere <snarksliveshere@gmail.com>
2018-09-01 23:52:13 +0300

    create frontend vacancy.show

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,13 +12,15 @@ class PageController extends Controller
         $vacancies = Vacancy::with(['users' => function ($q) {
                                     $q->select('id', 'name');}])
                     ->wherePublish(1)
-                    ->select('id', 'user_id', 'title', 'description', 'email')->get();
+                    ->select('id', 'user_id', 'title', 'description', 'email', 'updated_at')->get();
         return view('frontend.index', compact('vacancies'));
     }
 
     public function vacanciesShow($id)
     {
-
+        $vacancy = Vacancy::with(['users' => function ($q) {
+            $q->select('id', 'name');}])
+            ->whereId($id)->select('id', 'user_id', 'title', 'description', 'email', 'updated_at')->firstOrFail();
+        return view('frontend.vacancy.show', compact('vacancy'));
     }
-
 }
---
+++ resources/views/frontend/vacancy/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.frontend')
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <h2 class="card-header">{{ $vacancy->title }}</h2>
+            <h3 class="text-muted">Автор: {{ $vacancy->users->name }}</h3>
+            <p class="card-subtitle my-3 text-muted">Email: {{ $vacancy->email }}, Дата публикации: {{ $vacancy->updated_at }}</p>
+            <p class="card-text mb-2">Описание: {{ $vacancy->description }}</p>
+        </div>
+    </div>
+@endsection
