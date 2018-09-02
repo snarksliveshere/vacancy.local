d06870ed0e2fbcc32b8628bcba45e8c7263efc90
snarksliveshere <snarksliveshere@gmail.com>
2018-09-02 16:02:17 +0300

    add 404 page & 403 page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.frontend')
+@section('content')
+    <div class="container">
+        <h1>Ошибка 403</h1>
+        <h2>Вы не авторизованы для выполнения этого действия</h2>
+    </div>
+@endsection
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,7 @@
-<h1>404</h1>
+@extends('layouts.frontend')
+@section('content')
+    <div class="container">
+        <h1>Ошибка 404</h1>
+        <h2>Такой страницы не найдено</h2>
+    </div>
+@endsection
